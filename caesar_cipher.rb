07c799fc8724d6dd0538e80047fad8d97f777b51
Chris Bolas <tanlaan33@gmail.com>
2021-02-25 11:58:08 -0800

    Added more tests, split into functions

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,62 @@
 def caesar_cipher(phrase, shift=0)
-  phrase
+  # split string to characters
+  # map through each character
+  #   lowercase
+  #   uppercase
+  # reduce array back down to string 
+  # return
+  
+  if shift % 26 == 0 
+    return phrase
+  end
+
+  cipher = phrase.split('').map do |char|
+    shift_char(char, shift)
+  end
+
+  cipher.reduce('') {|word, char| word.concat(char)}
 end
+
+def shift_char(char, shift)
+  if char < 'a' || char > 'z' and char < 'A' || char > 'Z'
+    char
+  else
+    if char == char.downcase
+      shift_lowercase(char, shift)
+    else
+      shift_uppercase(char, shift)
+    end
+  end
+end
+
+def shift_lowercase(char, shift)
+  if shift.positive?
+    if 'z'.ord - char.ord < shift
+      shift - ('z'.ord - char.ord + 1) + 'a'.ord
+    else
+      char.ord + shift
+    end
+  else
+    if 'a'.ord - char.ord > shift
+      shift - ('a'.ord - char.ord - 1) + 'z'.ord
+    else
+      char.ord + shift
+    end
+  end
+end
+
+def shift_uppercase(char, shift)
+  if shift.positive?
+    if 'Z'.ord - char.ord < shift
+      shift - ('Z'.ord - char.ord + 1) + 'A'.ord
+    else
+      char.ord + shift
+    end
+  else
+    if 'A'.ord - char.ord > shift
+      shift - ('A'.ord - char.ord - 1) + 'Z'.ord
+    else
+      char.ord + shift
+    end
+  end
+end 
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -31,4 +31,53 @@ RSpec.describe 'Caesar Cipher Function' do
     end
   end
 
+  describe 'Positive shift functionality' do
+
+    it 'Returns a lowercase string' do
+      expect(caesar_cipher('abc', 1)).to eq('bcd')
+    end
+
+    it 'Returns an uppercase string' do
+      expect(caesar_cipher('ABC', 1)).to eq('BCD')
+    end
+
+    it 'Rounds the alphabet on lowercase' do
+      expect(caesar_cipher('z', 1)).to eq('a')
+    end
+
+    it 'Rounds the alphabet on uppercase' do
+      expect(caesar_cipher('Z', 1)).to eq('A')
+    end
+
+  end
+
+  describe 'Negative shift functionality' do
+
+    it 'Returns a lowercase string' do
+      expect(caesar_cipher('bcd', -1)).to eq('abc')
+    end
+
+    it 'Returns an uppercase string' do
+      expect(caesar_cipher('BCD', -1)).to eq('ABC')
+    end
+
+    it 'Rounds the alphabet on lowercase' do
+      expect(caesar_cipher('a', -1)).to eq('z')
+    end
+
+    it 'Rounds the alphabet on uppercase' do
+      expect(caesar_cipher('A', -1)).to eq('Z')
+    end
+  end
+
+  describe 'Non alphanumeric functioanlity' do
+    it 'Ignores spaces' do
+      expect(caesar_cipher('a b c', 1)).to eq('b c d')
+    end
+
+    it 'Ignores punctuation' do
+      expect(caesar_cipher('What a string!', 5)).to eq("Bmfy f xywnsl!")
+    end
+  end
+
 end
